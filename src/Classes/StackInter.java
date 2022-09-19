5857bb1185b471862fe1cbb4e912cb744081fad3
nojussta <nojus.stasiunas2@gmail.com>
2022-09-19 21:47:32 +0300

    add/updates-to-stack-intefrace

--- src/Classes/StackInter.java
+++ src/Classes/StackInter.java
@@ -1,2 +1,116 @@
-package Classes;public interface StackInter {
+package Classes;
+
+public interface StackInter<E> {
+    public void push(E item);
+
+    public E pop();
+
+
+    boolean isEmpty();
+
+    public E peek();
 }
+
+class ArrayStack<E> implements StackInter<E> {
+    @Override
+    public void push(E item) {
+
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+}
+
+class LinkedListStack<E> implements StackInter<E> {
+    @Override
+    public void push(E item) {
+
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+}
+
+class LinkedListStackImplementation<E> implements StackInter<E> {
+
+    private class Node<E> {
+
+        private E element;
+        private Node<E> next;
+
+        Node(E data, Node<E> next) {
+            this.element = data;
+            this.next = next;
+        }
+
+        public Node() {
+
+        }
+    }
+
+    Node<E> first;
+
+    @Override
+    public void push(E x) {
+        Node temp = new Node();
+
+        if (temp == null) {
+            System.out.print("\nHeap Overflow");
+            return;
+        }
+        temp.element = x;
+        temp.next = first;
+        first = temp;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    @Override
+    public E peek() {
+        if (!isEmpty()) {
+            return first.element;
+        } else {
+            System.out.println("Stack is empty");
+            return null;
+        }
+    }
+
+    @Override
+    public E pop() {
+        if (first == null) {
+            System.out.print("\nStack Underflow");
+            return null;
+        }
+
+        first = first.next;
+        return null;
+    }
+}
+
