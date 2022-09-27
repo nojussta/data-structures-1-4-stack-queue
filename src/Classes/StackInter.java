50f976445ba208f22055b5e7e75f4407fecc3dc2
nojussta <nojus.stasiunas2@gmail.com>
2022-09-27 15:52:06 +0300

    add/updates-to-interfaces

--- src/Classes/StackInter.java
+++ src/Classes/StackInter.java
@@ -3,7 +3,6 @@ package Classes;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.Queue;
 
 public interface StackInter<E> {
     public void push(E item);
@@ -14,74 +13,6 @@ public interface StackInter<E> {
     boolean isEmpty();
 
     public E peek();
-}
-
-class ArrayStack<E> implements StackInter<E> {
-    private ArrayList<E> thisList = new ArrayList<>();
-
-    public ArrayStack() {
-        this.thisList = new ArrayList<>();
-    }
-
-    public ArrayList<E> getCarList() {
-        return this.thisList;
-    }
-
-    @Override
-    public void push(E item) {
-        thisList.add(item);
-    }
-
-    @Override
-    public E pop() {
-        if (!isEmpty()) {
-            return thisList.remove(thisList.size() - 1);
-        }
-        return null;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return thisList.size() == 0;
-    }
-
-    @Override
-    public E peek() {
-        return thisList.get(thisList.size() - 1);
-    }
-}
-
-class LinkedListStack<E> implements StackInter<E> {
-    private LinkedList<E> thisList = new LinkedList<E>();
-
-    public LinkedListStack() {
-        this.thisList = new LinkedList<>();
-    }
-
-    public LinkedList<E> getCarList() {
-        return this.thisList;
-    }
-
-    @Override
-    public E pop() {
-        E removedElement = thisList.pop();
-        return removedElement;
-    }
-
-    @Override
-    public void push(E item) {
-        thisList.push(item);
-    }
-
-    @Override
-    public E peek() {
-        return thisList.peekFirst();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return thisList.isEmpty();
-    }
 }
 
 class LinkedListStackImplementation<E> implements StackInter<E> {
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import Classes.ArrayStack;
+import Classes.LinkedListStack;
+import Classes.QueueInter;
+import Classes.StackInter;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static <E> void main(String[] args) {
+        System.out.println("===============================Array stack===============================");
+        ArrayStack<Integer> arrayStack = new ArrayStack<Integer>();
+        arrayStack.push(1);
+        arrayStack.push(2);
+        arrayStack.push(3);
+        arrayStack.push(4);
+        arrayStack.push(5);
+        arrayStack.push(6);
+
+        while (!arrayStack.isEmpty()) {
+            System.out.println(arrayStack.peek());
+            System.out.println(arrayStack.pop());
+        }
+
+        System.out.println("===============================Linkedlist stack===============================");
+        LinkedListStack<Integer> linkedListStack = new LinkedListStack<Integer>();
+        linkedListStack.push(1);
+        linkedListStack.push(2);
+        linkedListStack.push(3);
+        linkedListStack.push(4);
+        linkedListStack.push(5);
+        linkedListStack.push(6);
+        while (!linkedListStack.isEmpty()) {
+            System.out.println(linkedListStack.peek());
+            System.out.println(linkedListStack.pop());
+        }
     }
 }
